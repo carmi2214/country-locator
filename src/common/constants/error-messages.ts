25121def27db262096281f02042156f59e233765
carmi2214 <carmirazzo@gmail.com>
2022-02-23 22:39:32 +0200

    fix: error message syntax

--- src/common/constants/error-messages.ts
+++ src/common/constants/error-messages.ts
@@ -1,4 +1,4 @@
 export const
-    COORDINATE_VALIDATION_ERROR_MESSAGE = `Invalid arguments were given. Must send either a point as array or lat & long as numbers.`,
+    COORDINATE_VALIDATION_ERROR_MESSAGE = `Invalid arguments were given. Must send either a point as an array or lat & long as numbers.`,
     POLYGON_VALIDATION_ERROR_MESSAGE = "Failed validating polygon. Validate that the polygon is an array of vertices. " +
         "Each vertex of the polygon must be represented as an array of [x: number, y: number]";
